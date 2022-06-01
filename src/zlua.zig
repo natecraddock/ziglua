6e178770c13a781a163858571bf64c848a3bba9c
Nathan Craddock <nzcraddock@gmail.com>
2022-05-31 20:04:13 -0600

    add standard library opening functions
    Exposes the luaopen_* and luaL_openlibs. Also adds a Ziggified wrapper
    function that takes a packed struct as input to specify which libs to
    load.
    
    After looking at rlua (a rust lua library) and the lua reference manual,
    it seems that these library loading functions have the possibility of
    failure. Not sure how to address this yet, but have left a todo.

--- src/zlua.zig
+++ src/zlua.zig
@@ -15,8 +15,6 @@ const Lua = struct {
     allocator: ?*Allocator = null,
     state: *c.lua_State,
 
-    // Ziggified wrapper functions
-
     /// Allows Lua to allocate memory using a Zig allocator passed in via data.
     /// See https://www.lua.org/manual/5.4/manual.html#lua_Alloc for more details
     fn alloc(data: ?*anyopaque, ptr: ?*anyopaque, osize: usize, nsize: usize) callconv(.C) ?*anyopaque {
@@ -102,6 +100,89 @@ const Lua = struct {
     }
 
     // Standard library loading functions
+    // TODO: opening libs can run arbitrary Lua code and can throw any error
+
+    pub const Libs = packed struct {
+        base: bool = false,
+        coroutine: bool = false,
+        package: bool = false,
+        string: bool = false,
+        utf8: bool = false,
+        table: bool = false,
+        math: bool = false,
+        io: bool = false,
+        os: bool = false,
+        debug: bool = false,
+    };
+
+    /// Opens the specified standard library functions
+    pub fn open(lua: *Lua, libs: Libs) void {
+        if (libs.base) lua.openBase();
+        if (libs.coroutine) lua.openCoroutine();
+        if (libs.package) lua.openPackage();
+        if (libs.string) lua.openString();
+        if (libs.utf8) lua.openUtf8();
+        if (libs.table) lua.openTable();
+        if (libs.math) lua.openMath();
+        if (libs.io) lua.openIO();
+        if (libs.os) lua.openOS();
+        if (libs.debug) lua.openDebug();
+    }
+
+    /// Open all standard libraries
+    pub fn openLibs(lua: *Lua) void {
+        c.luaL_openlibs(lua.state);
+    }
+
+    /// Open the basic standard library
+    pub fn openBase(lua: *Lua) void {
+        _ = c.luaopen_base(lua.state);
+    }
+
+    /// Open the coroutine standard library
+    pub fn openCoroutine(lua: *Lua) void {
+        _ = c.luaopen_coroutine(lua.state);
+    }
+
+    /// Open the package standard library
+    pub fn openPackage(lua: *Lua) void {
+        _ = c.luaopen_package(lua.state);
+    }
+
+    /// Open the string standard library
+    pub fn openString(lua: *Lua) void {
+        _ = c.luaopen_string(lua.state);
+    }
+
+    /// Open the UTF-8 standard library
+    pub fn openUtf8(lua: *Lua) void {
+        _ = c.luaopen_utf8(lua.state);
+    }
+
+    /// Open the table standard library
+    pub fn openTable(lua: *Lua) void {
+        _ = c.luaopen_table(lua.state);
+    }
+
+    /// Open the math standard library
+    pub fn openMath(lua: *Lua) void {
+        _ = c.luaopen_math(lua.state);
+    }
+
+    /// Open the io standard library
+    pub fn openIO(lua: *Lua) void {
+        _ = c.luaopen_io(lua.state);
+    }
+
+    /// Open the os standard library
+    pub fn openOS(lua: *Lua) void {
+        _ = c.luaopen_os(lua.state);
+    }
+
+    /// Open the debug standard library
+    pub fn openDebug(lua: *Lua) void {
+        _ = c.luaopen_debug(lua.state);
+    }
 };
 
 // Tests
@@ -135,4 +216,36 @@ test "initialization" {
     // use the auxiliary library
     lua = try Lua.auxNewState();
     lua.close();
+}
+
+test "standard library loading" {
+    // open a subset of standard libraries with Zig wrapper
+    {
+        var lua = try Lua.init(testing.allocator);
+        defer lua.deinit();
+        lua.open(.{ .base = true, .utf8 = true, .string = true });
+    }
+
+    // open all standard libraries
+    {
+        var lua = try Lua.init(testing.allocator);
+        defer lua.deinit();
+        lua.openLibs();
+    }
+
+    // open all standard libraries with individual functions
+    {
+        var lua = try Lua.init(testing.allocator);
+        defer lua.deinit();
+        lua.openBase();
+        lua.openCoroutine();
+        lua.openPackage();
+        lua.openString();
+        lua.openUtf8();
+        lua.openTable();
+        lua.openMath();
+        lua.openIO();
+        lua.openOS();
+        lua.openDebug();
+    }
 }
