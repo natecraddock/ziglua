794d32116a96ec6d4b841d7c8eee0fb398db1a6b
evan <evanbonner99@gmail.com>
2023-07-28 16:01:23 +0100

    Update include paths for all zig bindings

--- src/ziglua-5.1/lib.zig
+++ src/ziglua-5.1/lib.zig
@@ -4,9 +4,9 @@
 const std = @import("std");
 
 const c = @cImport({
-    @cInclude("lua.h");
-    @cInclude("lualib.h");
-    @cInclude("lauxlib.h");
+    @cInclude("lua/lua.h");
+    @cInclude("lua/lualib.h");
+    @cInclude("lua/lauxlib.h");
 });
 
 const Allocator = std.mem.Allocator;
