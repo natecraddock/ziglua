84113ade2cd8f586f03cbed9b3eebe33511d08bf
Nathan Craddock <nzcraddock@gmail.com>
2023-12-23 16:13:33 -0700

    feat: add initial version of Luau
    Still a work in progress, but the basics work now

---
+++ src/zigluau/assert.cpp
@@ -0,0 +1,13 @@
+#include "Luau/Common.h"
+
+#include <cstdio>
+
+static int assertionHandler(const char* expr, const char* file, int line, const char* function)
+{
+    printf("%s(%d): ASSERTION FAILED: %s\n", file, line, expr);
+    return 1;
+}
+
+extern "C" void zig_registerAssertionHandler() {
+    Luau::assertHandler() = assertionHandler;
+}
