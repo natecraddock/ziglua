c9d4a97cc8cb8226b12dcaf0b14556f0b93aa072
Robert Burnett <dr.slate2@gmail.com>
2024-02-06 20:57:30 -0600

    lua 51 compilation error fixes

--- src/tests.zig
+++ src/tests.zig
@@ -2501,12 +2501,12 @@ test "pushAny" {
 
     //int
     try lua.pushAny(1);
-    const my_int = try lua.toInteger(-1);
+    const my_int = try toInteger(&lua, -1);
     try testing.expect(my_int == 1);
 
     //float
     try lua.pushAny(1.0);
-    const my_float = try lua.toNumber(-1);
+    const my_float = try toNumber(&lua, -1);
     try testing.expect(my_float == 1.0);
 
     //bool
@@ -2556,26 +2556,24 @@ fn bar(a: i32, b: i32) !i32 {
 }
 
 test "autoPushFunction" {
-    if (ziglua.lang != .luau) {
-        var lua = try Lua.init(&testing.allocator);
-        defer lua.deinit();
-        lua.openLibs();
+    var lua = try Lua.init(&testing.allocator);
+    defer lua.deinit();
+    lua.openLibs();
 
-        lua.autoPushFunction(foo);
-        lua.setGlobal("foo");
+    lua.autoPushFunction(foo);
+    lua.setGlobal("foo");
 
-        lua.autoPushFunction(bar);
-        lua.setGlobal("bar");
+    lua.autoPushFunction(bar);
+    lua.setGlobal("bar");
 
-        try lua.doString("result = foo(1, 2)");
+    try lua.doString("result = foo(1, 2)");
 
-        const program =
-            \\local status, result = pcall(bar, 1, 2)
-        ;
-        lua.doString(program) catch |err| {
-            std.debug.print("{!}\n\n", .{err});
-        };
-    }
+    const program =
+        \\local status, result = pcall(bar, 1, 2)
+    ;
+    lua.doString(program) catch |err| {
+        std.debug.print("{!}\n\n", .{err});
+    };
 }
 
 test "autoCall" {
