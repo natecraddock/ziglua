be1990daa32df21e5b094d755eb3b70704ab0c5f
Robbie Lyman <rb.lymn@gmail.com>
2024-11-08 21:37:37 -0500

    fix: remove `Lua.equal` with compile error for lua 5.2, 5.3, 5.4 (#118)
    * fix: remove `Lua.equal` with compile error for lua 5.2, 5.3, 5.4
    
    `lua_equal` is deprecated in Lua versions 5.2 and later; the same
    behavior can be achieved with `lua_compare` with the equality
    flag. this commit makes using `lua_equal` a compile error when `lang`
    is 5.2, 5.3 or 5.4. in fact, it is already a compile error (since the
    C header does not expose the function), but making it fail faster with
    a helpful replacement suggestion seems useful.

--- src/lib.zig
+++ src/lib.zig
@@ -980,6 +980,10 @@ pub const Lua = opaque {
     ///
     /// See https://www.lua.org/manual/5.1/manual.html#lua_equal
     pub fn equal(lua: *Lua, index1: i32, index2: i32) bool {
+        switch (lang) {
+            .lua52, .lua53, .lua54 => @compileError("lua_equal is deprecated, use Lua.compare with .eq instead"),
+            else => {},
+        }
         return c.lua_equal(@ptrCast(lua), index1, index2) == 1;
     }
 
